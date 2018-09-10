4021e7eb1411ba08bf6a88217db27aa9bed88a31
Felipe Machado <felipelm123@gmail.com>
2018-09-10 16:12:42 -0300

    Updating readme (#3)
    * Updating readme
    
    * update readme
    
    * changing default port
    
    * adding empty line
    
    * update react-scripts
    
    * route fix
    
    * update entrypoint
    
    * update build
    
    * install dependencies
    
    * adding env template
    
    * update package
    
    * change endpoint
    
    * change endpoints
    
    * update deploy

--- src/index.js
+++ src/index.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  Router,
-  Route,
-  IndexRoute,
-  browserHistory,
-  Redirect,
-} from 'react-router';
+import { Router, Route } from 'react-router-dom';
+
+import createHistory from 'history/createBrowserHistory';
 
 import App from './containers/App';
-
-import Dashboard from './views/Dashboard';
 
 import 'primereact/resources/themes/omega/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 import registerServiceWorker from './registerServiceWorker';
+const history = createHistory();
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Redirect from="/App" to="/Dashboard" />
-      <IndexRoute component={Dashboard} />
-      <Route path="/Dashboard" component={Dashboard} />
-    </Route>
+  <Router basename={process.env.REACT_APP_PUBLIC_URL} history={history}>
+    <Route path={`${process.env.PUBLIC_URL}/`} component={App} />
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 registerServiceWorker();
