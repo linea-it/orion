b64218522a3442e376ff30169f62fe1989afe1e5
EricFreireSouza <ericfreire@gmail.com>
2018-12-14 15:43:02 -0200

    Add loading and zero tables when selecting new release

--- src/components/ReleaseFilter.js
+++ src/components/ReleaseFilter.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-import Proptypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { FadeLoader } from 'halogenium';
 
 import Centaurus from '../api';
 
-export default class ReleaseFilter extends Component {
+const styles = {
+  fadeLoaderFull: {
+    position: 'absolute',
+    paddingLeft: 'calc((100vw - 40px) / 2)',
+    paddingTop: 'calc(25vh)',
+  },
+  fadeLoader: {
+    position: 'absolute',
+    paddingLeft: 'calc((100vw - 13px) / 2)',
+    paddingTop: 'calc(35vh)',
+    zIndex: '999',
+  },
+};
+
+class ReleaseFilter extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -11,32 +27,49 @@ export default class ReleaseFilter extends Component {
       optsRelease: [],
       optsFields: [],
       pipelineStages: [],
+      selectRelease: '',
+      selectFild: '',
     };
   }
 
   static propTypes = {
-    saveStage: Proptypes.func.isRequired,
+    classes: PropTypes.object,
+    saveStage: PropTypes.func.isRequired,
   };
 
   componentDidMount() {
-    this.setState({
-      loading: true,
-    });
-
     this.loadReleases();
   }
 
   handleChangeReleases = evt => {
+    const opts = this.state.optsRelease;
+    opts['selectRelease'] = evt.target.value;
+
+    this.setState(opts);
     this.loadFields(evt.target.value);
+    this.props.saveStage([]);
   };
 
   handleChangeFields = evt => {
+    this.setState({ SelectFild: evt.target.value });
     this.loadStage(evt.target.value);
+  };
+
+  clearFields = () => {
+    this.setState({ optsFields: [] });
   };
 
   /* Valid field */
   isValid = () => {
     return this.state.optsFields.length !== 0;
+  };
+
+  loadStart = () => {
+    this.setState({ loading: true });
+  };
+
+  loadEnd = () => {
+    this.setState({ loading: false });
   };
 
   loadReleases = async () => {
@@ -56,10 +89,9 @@ export default class ReleaseFilter extends Component {
       });
       this.setState({
         optsRelease,
-        loading: false,
       });
     } else {
-      this.setState({ loading: false });
+      return null;
     }
   };
 
@@ -76,10 +108,9 @@ export default class ReleaseFilter extends Component {
       });
       this.setState({
         optsFields,
-        loading: false,
       });
     } else {
-      this.setState({ loading: false, optsFields: [] });
+      this.clearFields();
     }
   };
 
@@ -99,8 +130,9 @@ export default class ReleaseFilter extends Component {
         };
       });
       this.loadTableStages(pipelineStages);
+      this.loadStart();
     } else {
-      this.setState({ loading: false });
+      return null;
     }
   };
 
@@ -118,48 +150,82 @@ export default class ReleaseFilter extends Component {
       })
     );
     this.props.saveStage(tableStage);
+    this.loadEnd();
+  };
+
+  renderLoading = () => {
+    const { classes } = this.props;
+
+    if (!this.state.loading) return null;
+    const showControls = this.state.optsRelease || this.state.optsFields;
+    const classLoading = showControls
+      ? styles.fadeLoader
+      : styles.fadeLoaderFull;
+
+    if (
+      this.state.optsRelease.length !== 0 &&
+      this.state.optsFields.length !== 0
+    ) {
+      return (
+        <div className={classes.loading}>
+          <FadeLoader
+            style={classLoading}
+            color="#424242"
+            size="16px"
+            margin="4px"
+          />
+        </div>
+      );
+    }
   };
 
   render() {
     return (
-      <form className="form-inline">
-        <label className="sr-only" htmlFor="inlineFormInput">
-          Release:
-        </label>
-        <select
-          className="form-control form-control-sm sel"
-          onChange={this.handleChangeReleases}
-        >
-          <option value="" />
-          <option value="all">All</option>
-          {this.state.optsRelease.map(opt => {
-            return (
-              <option key={opt.id} value={opt.tagId}>
-                {opt.releaseDisplayName}
-              </option>
-            );
-          })}
-        </select>
+      <div>
+        <form className="form-inline">
+          <label className="sr-only" htmlFor="inlineFormInput">
+            Release:
+          </label>
+          <select
+            className="form-control form-control-sm sel"
+            onChange={this.handleChangeReleases}
+            value={this.state.selectRelease}
+          >
+            <option value="" />
+            <option value="all">All</option>
+            {this.state.optsRelease.map(opt => {
+              return (
+                <option key={opt.id} value={opt.tagId}>
+                  {opt.releaseDisplayName}
+                </option>
+              );
+            })}
+          </select>
 
-        <label className="sr-only" htmlFor="inlineFormInputGroup">
-          Dataset:
-        </label>
-        <select
-          className="form-control form-control-sm sel"
-          onChange={this.handleChangeFields}
-          disabled={!this.isValid()}
-        >
-          <option value="" />
-          <option value="all">All</option>
-          {this.state.optsFields.map(opt => {
-            return (
-              <option key={opt.id} value={opt.releaseTagId}>
-                {opt.displayName}
-              </option>
-            );
-          })}
-        </select>
-      </form>
+          <label className="sr-only" htmlFor="inlineFormInputGroup">
+            Dataset:
+          </label>
+          <select
+            className="form-control form-control-sm sel"
+            onChange={this.handleChangeFields}
+            value={this.state.selectField}
+            disabled={!this.isValid()}
+          >
+            <option value="" />
+            <option value="all">All</option>
+            {this.state.optsFields.map(opt => {
+              return (
+                <option key={opt.id} value={opt.releaseTagId}>
+                  {opt.displayName}
+                </option>
+              );
+            })}
+          </select>
+        </form>
+        {this.renderLoading()}
+      </div>
     );
   }
 }
+
+export default withStyles(styles)(ReleaseFilter);
---
+++ src/components/dashboard/TableProcess.js
@@ -0,0 +1,234 @@
+import React, { Component } from 'react';
+
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+// import { MultiSelect } from 'primereact/multiselect';
+import Proptypes from 'prop-types';
+import { Button } from 'primereact/button';
+
+const styles = {
+  btnStatus: {
+    textAlign: 'center',
+    width: '100%',
+  },
+  btnRuns: {
+    margin: '0 auto',
+    width: '4em',
+    display: 'block',
+  },
+};
+
+export default class TableProcess extends Component {
+  constructor(props) {
+    super(props);
+
+    this.colOptionsTable = [];
+
+    const columns = [
+      {
+        field: 'release',
+        header: 'Release',
+      },
+      {
+        field: 'dataset',
+        header: 'Dataset',
+      },
+      {
+        field: 'process',
+        header: 'Process ID',
+      },
+      {
+        field: 'process',
+        header: 'Process ID',
+      },
+      {
+        field: 'start',
+        header: 'Start time',
+      },
+      {
+        field: 'end',
+        header: 'End time',
+      },
+      {
+        field: 'version',
+        header: 'Version',
+      },
+      {
+        field: 'duration',
+        header: 'Duration',
+      },
+      {
+        field: 'owner',
+        header: 'Owner',
+      },
+      {
+        field: 'status',
+        header: 'Status',
+        body: this.actionStatus,
+      },
+      {
+        field: 'saved',
+        header: 'Saved',
+      },
+      {
+        field: 'share',
+        header: 'Share',
+      },
+      {
+        field: 'published',
+        header: 'Published',
+      },
+      {
+        field: 'provenance',
+        header: 'Provenance',
+      },
+      {
+        field: 'comments',
+        header: 'Comments',
+      },
+      {
+        field: 'product',
+        header: 'Product Log',
+      },
+      {
+        field: 'products',
+        header: 'Products',
+      },
+      {
+        field: 'export',
+        header: 'Export',
+        body: this.actionRuns,
+      },
+    ];
+
+    this.state = {
+      cols: columns,
+      loading: false,
+    };
+
+    // for (const col of columns) {
+    //   this.colOptionsTable.push({
+    //     label: col.header,
+    //     value: col,
+    //   });
+    // }
+  }
+
+  static propTypes = {
+    title: Proptypes.string,
+    rows: Proptypes.array,
+  };
+
+  //   onColumnToggleTable = evt => {
+  //     this.setState({ cols: evt.value });
+  //   };
+
+  openRuns = rowData => {
+    window.open('http://www.linea.gov.br/', rowData, 'width=950, height=650');
+  };
+
+  onStatus = rowData => {
+    console.log('onStatus: ', rowData);
+  };
+
+  actionStatus = rowData => {
+    if (rowData.status === 'failure') {
+      return (
+        <Button
+          type="button"
+          label="Failure"
+          title="Failure"
+          className="ui-button-danger"
+          style={styles.btnStatus}
+          // disabled={true}
+          onClick={() => this.onStatus(rowData)}
+        />
+      );
+    } else if (rowData.status === 'invalid') {
+      return (
+        <Button
+          type="button"
+          label="Invalid"
+          title="Invalid"
+          className="ui-button-secondary"
+          style={styles.btnStatus}
+          disabled={true}
+          onClick={() => this.onStatus(rowData)}
+        />
+      );
+    } else {
+      return (
+        <Button
+          type="button"
+          label="Success"
+          title="Success"
+          className="ui-button-success"
+          style={styles.btnStatus}
+          onClick={() => this.onStatus(rowData)}
+        />
+      );
+    }
+  };
+
+  actionRuns = rowData => {
+    if (rowData.runs !== 0) {
+      return (
+        <Button
+          type="button"
+          label={rowData.runs}
+          title={rowData.runs}
+          className="ui-button-info"
+          style={styles.btnRuns}
+          onClick={() => this.openRuns(rowData)}
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+
+  render() {
+    const header = (
+      <div style={{ textAlign: 'left' }}>
+        <p>{this.props.title}</p>
+        {/* <MultiSelect
+          value={this.state.cols}
+          options={this.colOptionsTable}
+          onChange={this.onColumnToggleTable}
+        /> */}
+      </div>
+    );
+
+    const columns = this.state.cols.map((col, i) => {
+      return (
+        <Column
+          key={i}
+          field={col.field}
+          header={col.header}
+          sortable={true}
+          body={col.body}
+        />
+      );
+    });
+
+    return (
+      <DataTable
+        header={header}
+        value={this.props.rows}
+        resizableColumns={true}
+        columnResizeMode="expand"
+        reorderableColumns={true}
+        reorderableRows={true}
+        responsive={true}
+        selectionMode="single"
+        selection={this.state.selectedCar1}
+        onSelectionChange={e => this.setState({ selectedCar1: e.data })}
+        scrollable={true}
+        scrollHeight="200px"
+        loading={this.state.loading}
+      >
+        {columns}
+      </DataTable>
+    );
+  }
+}
