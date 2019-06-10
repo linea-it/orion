74a7ec3143642bf7ec78498ebd932e3aff17f083
EricFreireSouza <ericfreire@gmail.com>
2019-06-10 15:40:29 -0300

    Alteração no import do createBrowserHistory

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router-dom';
 
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 
 import App from './containers/App';
 
-// import 'primereact/resources/themes/omega/theme.css';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -14,7 +13,7 @@ import 'primeicons/primeicons.css';
 import './assets/css/dashboard.css';
 
 import registerServiceWorker from './registerServiceWorker';
-const history = createHistory();
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <Router basename={process.env.REACT_APP_PUBLIC_URL} history={history}>
