7bc76e04c8783fa97317c056f13f43c619d3fa1f
Eric Freire <ericfreire@gmail.com>
2019-06-11 10:42:10 -0300

    All (#20)
    * Novo padrão
    
    * Ajustes no resize das colunas
    
    * Alteração no z-index do footer
    
    * Criação de tooltip e correção do duration
    
    * Inclusão de tooltip e correção do duration no Stages
    
    * Ajustes nas dependencias
    
    * Remoção do style
    
    * Validação
    
    * v1.6.0
    
    * Atualização de dependencias do package.json
    
    * Alteração no import do createBrowserHistory
    
    * Updated Dockerfile
    
    * Added env-config.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router-dom';
 
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 
 import App from './containers/App';
 
-import 'primereact/resources/themes/omega/theme.css';
+import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 import './assets/css/dashboard.css';
 
 import registerServiceWorker from './registerServiceWorker';
-const history = createHistory();
+require('dotenv').config();
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <Router basename={process.env.REACT_APP_PUBLIC_URL} history={history}>
