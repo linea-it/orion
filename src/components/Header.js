f16c25f76f9c2dfe5a224a52f0d440286dee6980
Fernando Nicolau <fernando_nicolau@outlook.com>
2020-03-05 14:30:42 -0300

    feat: redirection to the portal home via the logo and icon.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, IconButton, Button } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
 import Logo from '../assets/img/icon-des.png';
 
 const styles = {
@@ -16,7 +17,15 @@ const styles = {
     boxShadow: 'none',
     zIndex: '99',
   },
+  separatorToolBar: {
+    flexGrow: 1,
+  },
 };
+
+const homeUrl =
+process.env.NODE_ENV === 'production'
+  ? window._env_.REACT_APP_HOME_URL
+  : process.env.REACT_APP_HOME_URL;
 
 class Header extends React.Component {
   static propTypes = {
@@ -30,17 +39,24 @@ class Header extends React.Component {
       <header className={classes.root}>
         <AppBar className={classes.AppBar} position="fixed">
           <Toolbar variant="dense">
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-            >
-              <img src={Logo} alt="Portal" />
-            </IconButton>
-
-            <Typography variant="h6" color="inherit">
-              DES Science Portal Dashboard
-            </Typography>
+              <IconButton
+                href={homeUrl}
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="Menu"
+                >
+                <img src={Logo} alt="Portal" />
+              </IconButton>
+              <Typography variant="h6" color="inherit">
+                Dashboard
+              </Typography>
+              <div className={classes.separatorToolBar} />
+              <Button
+                color="inherit"
+                size="extra-large"
+                href={homeUrl}
+                startIcon={<HomeIcon />}
+              />
           </Toolbar>
         </AppBar>
       </header>
