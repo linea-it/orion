23e776bbdb00fe523c6a761081e3a201ee55f769
Fernando <fernando_nicolau@outlook.com>
2020-03-24 12:18:05 -0300

    feat: Removing Primereact, applying CustomTable.

--- src/components/TableVersion/index.js
+++ src/components/TableVersion/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import CustomTable from '../Table/';
 
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
+// import { DataTable } from 'primereact/datatable';
+// import { Column } from 'primereact/column';
 
 import { withStyles } from '@material-ui/core/styles';
 
@@ -20,20 +21,23 @@ class TableVersion extends Component {
 
     const columns = [
       {
-        field: 'name',
-        header: 'Name',
+        name: 'name',
+        title: 'Name',
         style: {
           textAlign: 'left',
           paddingLeft: '20px',
         },
+        width: 300,
       },
       {
-        field: 'version',
-        header: 'Version',
+        name: 'version',
+        title: 'Version',
+        width: 300,
       },
       {
-        field: 'last_version',
-        header: 'Last Version',
+        name: 'last_version',
+        title: 'Last Version',
+        width: 300,
       },
     ];
 
@@ -50,36 +54,38 @@ class TableVersion extends Component {
   };
 
   render() {
-    const columns = this.state.cols.map((col, i) => {
-      return (
-        <Column
-          key={i}
-          field={col.field}
-          header={col.header}
-          // sortable={true}
-          style={col.style ? col.style : {}}
-          body={col.body}
-        />
-      );
-    });
-
     return (
-      <DataTable
-        value={this.props.versionProcess}
-        resizableColumns={true}
-        columnResizeMode="expand"
-        reorderableColumns={true}
-        reorderableRows={true}
-        responsive={true}
-        selectionMode="single"
-        selection={this.state.selectedCar1}
-        onSelectionChange={e => this.setState({ selectedCar1: e.data })}
-        scrollable={true}
-        scrollHeight="600px"
-        style={{ zIndex: '95' }}
-      >
-        {columns}
-      </DataTable>
+      <CustomTable
+        // eslint-disable-next-line react/prop-types
+        data={this.props.versionProcess}
+        columns={this.state.cols}
+        remote={false}
+        pageSize={5}
+        pageSizes={[5, 15, 50, 100, 500]}
+        // eslint-disable-next-line react/prop-types
+        totalCount={this.props.versionProcess.length}
+        reload={false}
+        hasSearching={false}
+        hasToolbar={false}
+        hasPagination={false}
+        hasColumnVisibility={false}
+      />
+      // <DataTable
+      //   value={this.props.versionProcess}
+      //   resizableColumns={true}
+      //   columnResizeMode="expand"
+      //   reorderableColumns={true}
+      //   reorderableRows={true}
+      //   responsive={true}
+      //   selectionMode="single"
+      //   selection={this.state.selectedCar1}
+      //   onSelectionChange={e => this.setState({ selectedCar1: e.data })}
+      //   scrollable={true}
+      //   scrollHeight="600px"
+      //   style={{ zIndex: '95' }}
+      // >
+      //   {columns}
+      // </DataTable>
     );
   }
 }
