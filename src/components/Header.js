a1b5cba107f53ffb96072aed34fd67246a3f7888
Fernando Nicolau <fernando_nicolau@outlook.com>
2020-03-25 19:27:57 -0300

    feat: redirection to the portal home via the logo and icon. (#42)
    * feat: redirection to the portal home via the logo and icon.
    
    * feat: using windows location at uri da hom.
    
    * chore: removing unused env.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, IconButton, Button } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
 import Logo from '../assets/img/icon-des.png';
 
 const styles = {
@@ -16,7 +17,14 @@ const styles = {
     boxShadow: 'none',
     zIndex: '99',
   },
+  separatorToolBar: {
+    flexGrow: 1,
+  },
 };
+
+const homeUrl = `${window.location.protocol}//${window.location.hostname}${
+  window.location.port ? ':' : ''
+}${window.location.port}`;
 
 class Header extends React.Component {
   static propTypes = {
@@ -30,17 +38,24 @@ class Header extends React.Component {
       <header className={classes.root}>
         <AppBar className={classes.AppBar} position="fixed">
           <Toolbar variant="dense">
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-            >
-              <img src={Logo} alt="Portal" />
-            </IconButton>
-
-            <Typography variant="h6" color="inherit">
-              DES Science Portal Dashboard
-            </Typography>
+              <IconButton
+                href={homeUrl}
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="Menu"
+                >
+                <img src={Logo} alt="Portal" />
+              </IconButton>
+              <Typography variant="h6" color="inherit">
+                Dashboard
+              </Typography>
+              <div className={classes.separatorToolBar} />
+              <Button
+                color="inherit"
+                size="extra-large"
+                href={homeUrl}
+                startIcon={<HomeIcon />}
+              />
           </Toolbar>
         </AppBar>
       </header>
